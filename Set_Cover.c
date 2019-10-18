2b44bc67de6af562925c113c03787a90fd05b61d
Nandan Pandey <pandeynandancse@gmail.com>
2019-10-18 14:28:57 +0530

    Add files via upload

---
+++ Set_Cover.c
@@ -0,0 +1,116 @@
+
+#include "stdio.h"
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAX_SET_MEMBERS 20
+
+//IN ANY SET AT INDEX [0] THE NO. OF ELEMENTS PRESENT IN THAT SET ARE STORED
+
+int **initializeSets(int no_of_sets)
+{
+	int **container = NULL, *set = NULL;
+	container = (int **)calloc(no_of_sets, sizeof(int *));
+
+	int input;
+	for(int i = 0; i < no_of_sets; i++)
+	{
+		printf("No. of elements in set [ %d ] >>> ", i);
+		scanf("%d", &input);
+
+		set = (int *)calloc(MAX_SET_MEMBERS, sizeof(int));
+		set[0] = input;
+		container[i] = set;
+
+		printf("Enter the %d elements of set [ %d ]\n", input, i);
+		for(int j = 1; j <= set[0]; j++)
+		{
+			scanf("%d", &input);
+			set[input] = TRUE;
+		}
+		printf("\n");
+	}
+
+	for(int i = 0; i < no_of_sets; i++)
+	{
+		set = container[i];
+		printf("\n Elements in Set [ %d ] >> ", i);
+		for(int j = 1; j < MAX_SET_MEMBERS; j++)
+			if(set[j] == TRUE) printf("%d, ", j);
+
+	}
+	printf("\n");
+	return container;
+}
+
+int find_set_containing_max_elements(int **container, int no_of_sets)
+{
+	int max_set_index = 1;
+	for(int i = 1; i < no_of_sets; i++)
+		if(container[i][0] > container[max_set_index][0])
+			max_set_index = i;
+
+	return max_set_index;
+}
+
+//	complexity can be reduced by taking the answer_set and 
+//	not visiting the answer_set which is already considered
+//	if(answer_set[j] == TRUE) continue; //not much complexity reduced --- even ERROR
+//	because making it TRUE before removing elements
+
+void remove_elements(int **container, int no_of_sets, int max_set_index)
+{
+	int *max_set = container[max_set_index];
+	for(int i = 1; i < MAX_SET_MEMBERS; i++)
+	{
+		if(max_set[i] == TRUE)
+		{
+			for(int j = 0; j < no_of_sets; j++)
+			{
+				if(container[j][i] == TRUE)
+				{
+					container[j][i] = FALSE;
+					container[j][0] = container[j][0] - 1;
+				}
+			}
+
+		}
+	}
+}
+
+void set_covering_approximation_algorithm(int **container, int no_of_sets)
+{
+	int *answer_set = (int *)calloc(no_of_sets, sizeof(int));
+	int max_set_index;
+
+	while(container[0][0] != 0)
+	{
+		max_set_index = find_set_containing_max_elements(container, no_of_sets);
+		answer_set[max_set_index] = TRUE;
+		remove_elements(container, no_of_sets, max_set_index);
+	}
+
+	printf("\n\nApprox. Answer Set for Set Covering Problem\n");
+	for(int i = 1; i < no_of_sets; i++)
+		if(answer_set[i] == TRUE) printf("SET [ %d ]\n", i);
+
+	free(answer_set);
+
+}
+
+int main()
+{
+	int no_of_sets = 0, **container = NULL;
+	printf("\nEnter No. of Sets >>> ");
+	scanf("%d", &no_of_sets);
+	
+	container = initializeSets(no_of_sets);
+
+	set_covering_approximation_algorithm(container, no_of_sets);
+
+	for(int i = 0; i < no_of_sets; i++) free(container[i]);
+	free(container);
+	printf("\n");
+	return 0;
+}
---
+++ Vertex_Cover_Approximation.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define VERTICES 20
+#define FALSE 0
+#define TRUE 1
+
+struct node 
+{
+	int vertex;
+	struct node *link;
+};
+
+struct node **createGraph(int vertices)
+{
+	struct node **graph = (struct node **)calloc(vertices, sizeof(struct node *));
+	struct node *temp, *last;
+	int i,j,option;
+	printf("[ YES -> 1 ]  OR  [ NO -> 0 ]\n");
+	for(i = 0; i < vertices; i++)
+	{
+		temp = last = NULL;
+		for(j = 0; j < vertices; j++)
+		{
+			if(i == j)
+				continue;
+
+			printf("[ %d ~ %d ] = ", i, j);
+			scanf("%d",&option);
+
+			if(option)
+			{
+				temp = (struct node *)malloc(sizeof(struct node));
+				temp->vertex = j;
+				temp->link = NULL;
+				if(last == NULL) graph[i] = temp;
+				else last->link = temp;
+				last = temp;
+			}
+		}
+	}
+	return graph;
+}
+
+
+
+/*
+Complexity would be O( |V| * |E| )
+worst case (complete graph) O( N^3 ) --> polynomial
+because the graph is untouched
+*/
+void VertexCoverApproximation(struct node **graph, int vertices)
+{
+	int i, answerSet[VERTICES] = {};
+	struct node *edge = NULL;
+	for(i = 0; i < vertices; i++)
+	{
+		if(answerSet[i] == FALSE) 
+		{
+			edge = graph[i]; //find that edge which is not covered and include it
+			while(edge && answerSet[edge->vertex] == TRUE) edge = edge->link; 
+			if(edge) answerSet[i] = answerSet[edge->vertex] = TRUE;
+		}
+	}
+
+	printf("\nAPPROXIMATION OF VERTICES IN VERTEX COVER SET\n");
+	for(i = 0; i < vertices; i++)
+		if(answerSet[i] == TRUE) printf("[ %d ]\n", i);
+}
+
+
+
+int main()
+{
+	int vertices;
+	printf("\nNumber of vertices :: ");
+	scanf("%d",&vertices);
+
+	struct node **graph = NULL;
+	graph = createGraph(vertices);
+	
+	VertexCoverApproximation(graph, vertices);
+
+	printf("\n\n");
+
+	return 0;
+	
+}
